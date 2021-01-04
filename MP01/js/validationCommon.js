f384ee7c0cf532377a405c773ff737c22da4df2a
karol <s17580@pjwstk.edu.pl>
2021-01-04 02:08:57 +0100

    finish validationSamochodForm

--- MP01/js/validationCommon.js
+++ MP01/js/validationCommon.js
@@ -1,6 +1,6 @@
 function resetErrors(inputs, errorTexts, errorInfo) {
     for(let i=0; i<inputs.length; i++) {
-        inputs[i].classList.remove("error_input");
+        inputs[i].classList.remove("error-input");
     }
     for(let i=0; i<errorTexts.length; i++) {
         errorTexts[i].innerText = "";
