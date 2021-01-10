83ff48e8bd9f8e678f95694a61505fda99571d7a
karol <s17580@pjwstk.edu.pl>
2021-01-10 22:32:11 +0100

    Finish validation

---
+++ MP01/js/validationLogowanie.js
@@ -0,0 +1,35 @@
+const usernameInput = document.getElementById("username");
+const errorUsername = document.getElementById("errorUsername");
+const passwordInput = document.getElementById("password");
+const errorPassword = document.getElementById("errorPassword");
+
+const errorsSummary = document.getElementById("errorsSummary");
+
+function validateForm() {
+
+    resetErrors([usernameInput, passwordInput],[errorUsername, errorPassword], errorsSummary);
+    let valid = true;
+    if (!checkRequired(usernameInput.value)) {
+      valid = false;
+      usernameInput.classList.add("error-input");
+      errorUsername.innerText = "Pole jest wymagane";
+    } else if (!checkTextLengthRange(usernameInput.value, 2, 15)) {
+      valid = false;
+      usernameInput.classList.add("error-input");
+      errorUsername.innerText = "Pole powinno zawierać od 2 do 15 znaków";
+  }  
+  if (!checkRequired(passwordInput.value)) {
+    valid = false;
+    passwordInput.classList.add("error-input");
+    errorPassword.innerText = "Pole jest wymagane";
+  } else if (!checkPassword(passwordInput.value)) {
+    valid = false;
+    passwordlInput.classList.add("error-input");
+    errorPassword.innerText = "Hasło powinno mieć min 8 znaków, jedną dużą literę, jedną liczbę i znak specjalny";
+}
+    if (!valid) {
+      errorsSummary.innerText = "Formularz zawiera błędy";
+    }
+
+    return valid;
+  }
--- MP01/js/validationRejestracja.js
+++ MP01/js/validationRejestracja.js
@@ -1,14 +1,18 @@
 const usernameInput = document.getElementById("username");
 const errorUsername = document.getElementById("errorUsername");
-// const typInput = document.getElementById("typ");
-// const errorTyp = document.getElementById("errorTyp");
+const emailInput = document.getElementById("email");
+const errorEmail = document.getElementById("errorEmail");
+const passwordInput = document.getElementById("password");
+const errorPassword = document.getElementById("errorPassword");
+const password2Input = document.getElementById("password2");
+const errorPassword2 = document.getElementById("errorPassword2");
 
 
 const errorsSummary = document.getElementById("errorsSummary");
 
 function validateForm() {
 
-    resetErrors([usernameInput],[errorUsername], errorsSummary);
+    resetErrors([usernameInput,emailInput, passwordInput,password2Input],[errorUsername,errorEmail,errorPassword,errorPassword2], errorsSummary);
     let valid = true;
     if (!checkRequired(usernameInput.value)) {
       valid = false;
@@ -18,8 +22,30 @@ function validateForm() {
       valid = false;
       usernameInput.classList.add("error-input");
       errorUsername.innerText = "Pole powinno zawierać od 2 do 15 znaków";
-	}
-	
+  }
+  if (!checkRequired(emailInput.value)) {
+    valid = false;
+    emailInput.classList.add("error-input");
+    errorEmail.innerText = "Pole jest wymagane";
+} else if (!checkTextLengthRange(emailInput.value, 5, 40)) {
+    valid = false;
+    emailInput.classList.add("error-input");
+    errorEmail.innerText = "Pole powinno zawierać od 5 do 40 znaków";
+} else if (!checkEmail(emailInput.value)) {
+    valid = false;
+    emailInput.classList.add("error-input");
+    errorEmail.innerText = "Pole powinno zawierać prawidłowy adres email";
+}
+if (!checkRequired(passwordInput.value)) {
+  valid = false;
+  passwordInput.classList.add("error-input");
+  errorPassword.innerText = "Pole jest wymagane";
+}
+  if (!checkRequired(password2Input.value)) {
+    valid = false;
+    password2Input.classList.add("error-input");
+    errorPassword2.innerText = "Pole jest wymagane";
+  }
 
     if (!valid) {
       errorsSummary.innerText = "Formularz zawiera błędy";
